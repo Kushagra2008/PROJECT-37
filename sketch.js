c4b8f5c8ff42c4a66879ff0719c711fa6e335e80
Kushagra2008 <73027622+Kushagra2008@users.noreply.github.com>
2020-12-24 20:30:30 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -23,10 +23,10 @@ function preload()
   dogImg1 = loadImage('dogImg.png');
   dogImg2 = loadImage('dogImg1.png');
 
-  bg1 = loadImage("images/Bed Room.png")
-  bg2 = loadImage("images/Garden.png")
-  bg3 = loadImage("images/Living Room.png")
-  bg4 = loadImage("images/Wash Room.png");
+  bg1 = loadImage("Bed Room.png")
+  bg2 = loadImage("Garden.png")
+  bg3 = loadImage("Living Room.png")
+  bg4 = loadImage("Wash Room.png");
 }
 
 function setup() {
@@ -195,74 +195,4 @@ function remove()
   dogSprite.x = 2000;
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-  //   if (keyWentDown(UP_ARROW) && TIMES < 5)
-  //   {
-  //     writeStock(foodS)
-  //     TIMES += 1;
-  //     dogSprite.addImage(dogImg2)
-  //     dogSprite.scale = 0.4;
-  //   }
-
-  // else if (keyWentDown(UP_ARROW) && TIMES >= 5)
-  //     {
-  //       textVal = "";
-  //       barksound.play()
-  //       text("translating", width/ 2, height / 2);
-  //       sleep(2000);
-
-  //       // TRANSLATING DOG LANGUAGE TO ENGLISH
-  //         robotSound.play();
-  //         sleep(2000);
-  //       textVal = "HEY, I AM NOT HUNGRY NOW!"
-  //     }
-  
-
-  // if (gamestate == 0)
-  // {
-  //   a = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-  //   rect(10, height/2 - 40, width - 20, 40)
-  //   for (var x = 0; x < 10; x++)
-  //   {
-  //     if (keyWentDown())
-  //     {
-        
-  //     }
-  //   }
-  //   if (keyWentDown("enter"))
-  //   {
-  //     gamestate = 1;
-  //   }
-  // }
-
-  // function writeStock(x)
-  // {
-  //   if (x <= 0)
-  //   {
-  //     x = 0
-  //   }
-  //   else
-  //   {
-  //     x -= 1
-  //   }
-  //   console.log(x)
-  //   remainingFood = x;
-  //   reference = database.ref('/')
-  //   reference.update({
-  //     Food:x
-  //   })
-  // }
-  
-  // 
     
